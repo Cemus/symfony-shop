0e6e8bb27b4e68405fca419db234f4375bf126af
Cemus <24947582+Cemus@users.noreply.github.com>
2025-03-05 16:59:26 +0100

    Feat : articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,16 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
+    public function __construct(
+        private readonly ArticleRepository $articleController
+    ) {}
     #[Route(path: '/articles',name:'app_articles')]
-    public function allArticles(): Response
+    public function showAllArticles(): Response
     {
-        return $this->render('articles.html.twig', []);
+        return $this->render('articles.html.twig', [
+            'articles' => $this->articleController->findAll(),
+        ]);
     }
     #[Route(path: '/detail',name:'app_detail')]
     public function articleId(): Response
