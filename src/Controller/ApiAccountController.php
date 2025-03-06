d9779dc4d28bb826e18eada1de7a6f0c3d8e1a60
Cemus <24947582+Cemus@users.noreply.github.com>
2025-03-06 11:41:23 +0100

    Feat : Add api/accounts

---
+++ src/Controller/ApiAccountController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AccountRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ApiAccountController extends AbstractController
+{
+    public function __construct(
+        private AccountRepository $accountRepository
+    ) {}
+
+    #[Route('/api/accounts', name: 'api_account_all')]
+    public function getAllAccount(): Response
+    {
+        return $this->json(
+            $this->accountRepository->findAll(),
+            200,
+            [],
+            ['groups' => 'account:read']
+        );
+    }
+}
