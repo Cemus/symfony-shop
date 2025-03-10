ddc5ef014fb6ddb9d3cf43ef57adca2ade346662
Cemus <24947582+Cemus@users.noreply.github.com>
2025-03-10 14:56:06 +0100

    Feat : AddAccout page

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Account;
+use App\Form\AccountType;
 use App\Repository\AccountRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AccountController extends AbstractController
 {
     public function __construct(
-        private readonly AccountRepository $accountRepository
+        private readonly AccountRepository $accountRepository,
+        private readonly EntityManagerInterface $em
     ) {}
 
     #[Route('/accounts', name: 'app_accounts')]
@@ -19,6 +24,33 @@ final class AccountController extends AbstractController
 
         return $this->render('accounts.html.twig', [
             'accounts' => $this->accountRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/add-account', name: 'app_add_account')]
+    public function addCategory(Request $request): Response
+    {
+        $account = new Account();
+        $form = $this->createForm(AccountType::class,$account );
+        $form->handleRequest($request);
+        $msg = "";
+        $status= "";
+
+        if($form->isSubmitted()){
+            try {
+                $this->em->persist($account);
+                $this->em->flush();
+                $msg = "Le compte a été ajouté avec succès";
+                $status = "success";
+            } catch (\Exception $e) {
+                $msg ="Le compte existe déja";
+                $status = "danger";
+            }
+        }
+        $this->addFlash($status, $msg);
+        return $this->render('addAccount.html.twig',
+        [
+            'form'=> $form // ->createView() ; optionnel
         ]);
     }
 }
--- src/Form/AccountType.php
+++ src/Form/AccountType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Account;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +16,41 @@ class AccountType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')
-            ->add('password')
-            ->add('roles')
-        ;
+            ->add('firstname', TextType::class,[
+                'label' => 'Prénom',
+                'attr'=>[
+                    'placeholder' => 'Saisir votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class,[
+                'label' => 'Nom',
+                'attr'=>[
+                    'placeholder' => 'Saisir votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class,[
+                'label' => 'Courriel',
+                'attr'=>[
+                    'placeholder' => 'Saisir votre addresse mail'
+                ]
+            ])
+            ->add('password', PasswordType::class,[
+                'label' => 'Mot de passe',
+                'attr'=>[
+                    'placeholder' => 'Saisir votre mot de passe'
+                ]
+            ])
+            ->add('roles', TextType::class,[
+                'label' => 'Rôle',
+                'attr'=>[
+                    'placeholder' => 'Saisir le rôle de l\'utilisateur'
+                ]
+            ])   
+            ->add('save',SubmitType::class,[
+                'label' => 'Soumettre le compte'
+            ])
+            ;
+                 ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
